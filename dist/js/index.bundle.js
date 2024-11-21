5827cd8b6fdd2eeb3644550fed099a4a9452dda3
Vadim Komolov <vkomolov.ua@gmail.com>
2024-11-21 02:46:22 +0200

    to set scroll animations

--- dist/js/index.bundle.js
+++ dist/js/index.bundle.js
@@ -9622,6 +9622,7 @@ const i = {
   iconWrapperHero: ".section__img-wrapper--hero",
   fadeInLeft: "[data-type=fade-in-left]",
   fadeInRight: "[data-type=fade-in-right]",
+  fadeInUp: "[data-type=fade-in-up]",
   scaleIn: "[data-type=scale-in]"
 };
 
@@ -9683,156 +9684,190 @@ const pageAnimations = {
       ease: "elastic.out"
     }, "<+0.5");
     if (hasRealAnimations(tlHero)) {
-      Object.assign(tlData, {
-        tlHero
-      });
+      tlData["tlHero"] = tlHero;
     }
 
     ///////////// FADE-IN-LEFT ANIMATIONS /////////////////
 
-    const fadeInLeftElems = gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.utils.toArray(i.fadeInLeft);
-    fadeInLeftElems.forEach((elem, index) => {
-      const tlKey = `fadeInLeft_${index}`;
-      const tl = getScrollTimeLineFadeLeftRight(elem, "left");
-      if (tl && hasRealAnimations(tl)) {
-        Object.assign(tlData, {
-          [tlKey]: tl
-        });
-      }
+    const fadeInLeftAnimations = getAllScrollTwoTweens(i.fadeInLeft, "fadeInLeft", {
+      opacity: 1,
+      duration: 0.7,
+      delay: 0.2
+    }, {
+      x: -80,
+      y: 80,
+      duration: 1,
+      ease: "circ.out"
     });
+    //assigning timeline references
+    Object.assign(tlData, fadeInLeftAnimations);
 
     ///////////// FADE-IN-RIGHT ANIMATIONS /////////////////
 
-    const fadeInRightElems = gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.utils.toArray(i.fadeInRight);
-    fadeInRightElems.forEach((elem, index) => {
-      const tlKey = `fadeInRight_${index}`;
-      const tl = getScrollTimeLineFadeLeftRight(elem, "right");
-      if (tl && hasRealAnimations(tl)) {
-        Object.assign(tlData, {
-          [tlKey]: tl
-        });
-      }
+    const fadeInRightAnimations = getAllScrollTwoTweens(i.fadeInRight, "fadeInRight", {
+      opacity: 1,
+      duration: 0.7,
+      delay: 0.2
+    }, {
+      x: 80,
+      y: 80,
+      duration: 1,
+      ease: "circ.out"
     });
+    //assigning timeline references
+    Object.assign(tlData, fadeInRightAnimations);
 
     ///////////// SCALE-IN ANIMATIONS ///////////
-    const scaleInElems = gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.utils.toArray(i.scaleIn);
-    scaleInElems.forEach((elem, index) => {
-      const tlKey = `scaleIn_${index}`;
-      const tl = getScrollTimeLineScaleIn(elem);
-      if (tl && hasRealAnimations(tl)) {
-        Object.assign(tlData, {
-          [tlKey]: tl
-        });
-      }
-    });
 
-    ////////////
+    const scaleInAnimations = getAllScrollTwoTweens(i.scaleIn, "scaleIn", {
+      opacity: 1,
+      duration: 1,
+      delay: 0.2
+    }, {
+      scale: 0.7,
+      duration: 1.2,
+      ease: "back.out(1)"
+    });
+    //assigning timeline references
+    Object.assign(tlData, scaleInAnimations);
+
+    //////////// FADE-IN-UP ANIMATIONS /////////////////
+
+    const fadeInUpAnimations = getAllScrollTwoTweens(i.fadeInUp, "fadeInUp", {
+      opacity: 1,
+      duration: 1,
+      delay: 0.2
+    }, {
+      y: 80,
+      duration: 1,
+      ease: "circ.out"
+    });
+    //assigning timeline references
+    Object.assign(tlData, fadeInUpAnimations);
+
+    /////////////////
 
     return tlData;
   }
 };
 
-////////////////  ANIMATION FUNCTION ////////////////
+////////////////  ANIMATION FUNCTIONS ////////////////
 
 //for the dynamic and SPA site without reloading, using the flag to avoid multiple listeners
 let listenerAdded = false;
-function animatePage() {
+const animatePage = () => {
   if (document.readyState === "loading" && !listenerAdded) {
-    document.addEventListener("DOMContentLoaded", onPageLoaded);
+    document.addEventListener("DOMContentLoaded", () => onPageLoaded(pageAnimations));
     listenerAdded = true; // Set a flag so that the listener is added only once
   } else {
-    return onPageLoaded();
+    return onPageLoaded(pageAnimations);
   }
-  function onPageLoaded() {
-    const pageName = document.body.dataset.type;
-    const totalTl = pageAnimations.common();
-    if (pageName in pageAnimations) {
-      const tlPage = pageAnimations[pageName]();
-      return Object.assign(totalTl, tlPage);
-    } else {
-      console.warn(`no such page name ${pageName} found in "pageAnimations"...`);
-      return totalTl;
-    }
-  }
-}
-function hasRealAnimations(timeline) {
-  // Getting all child tweens of the timeline
-  const children = timeline.getChildren();
+};
 
-  // Checking if there is at least one tween with a duration greater than zero
-  return children.some(child => child.duration() > 0);
+/**
+ * It gets the animation instructions Object and returns the Object with the references to the initiated timelines
+ * @param {Object} animationData - the Object with the animation instructions for the pages
+ * @return {Object} - Returns the Object with the references to all the initiated timelines
+ */
+function onPageLoaded(animationData) {
+  const pageName = document.body.dataset.type;
+  const totalTl = {};
+  if (animationData.common) {
+    const commonTl = animationData.common();
+    Object.assign(totalTl, commonTl);
+  } else {
+    console.warn(`at onPageLoaded(): no "common" property found in the given Object...`);
+  }
+  if (pageName in animationData) {
+    const tlPage = animationData[pageName]();
+    return Object.assign(totalTl, tlPage);
+  } else {
+    console.warn(`at onPageLoaded(): no such page name property: ${pageName} found in the given Object...`);
+    return totalTl;
+  }
 }
 
 /**
- * It creates the Timeline scroll animations with fading from left or right
- * @param {HTMLElement} elem  - the HTMLElement
- * @param {("left", "right")} position - for left and right elements in sections "--pair"
- * @return {gsap.core.Timeline | false} - returns the object with the special key of the timeline or false
+ * Checking the gsap timeline for not empty animations
+ * @param {gsap.core.Timeline} timeline - the gsap timeline to be checked
+ * @return {boolean} - returns whether the timeline has at least one not empty tween
  */
-function getScrollTimeLineFadeLeftRight(elem, position) {
+function hasRealAnimations(timeline) {
+  // Getting all child tweens of the timeline
+  // Checking if there is at least one tween with a duration greater than zero
+  return timeline.getChildren().some(child => child.duration() > 0);
+}
+
+/**
+ * It creates the Timeline scroll animations with fading in from opacity: 0, and optional gsap.from params which
+ * are animated with the opacity animation...
+ * @param {HTMLElement} elem  - the HTMLElement
+ * @param {Object} [gsapToParams={}] - params for the first tween with gsap.to
+ * @param {Object} [gsapFromParams={}] - params for the last tween with gsap.from
+ * @param {String} [nextAnimePos="<"] - The position for the second tween, indicating when the animation should start relative to the first.
+ * @return {gsap.core.Timeline|boolean} - Returns a Timeline with animations, or false if the passed element is not an HTMLElement.
+ *
+ * @example using first tween gsap.to params for the first animation, and gsap.from params for the last tween
+ * getScrollTimelineTwoTweens(elem, { opacity: 1, duration: 0.7, delay: 0.2 }, { x: 80, y: 80, duration: 1, ease: "circ.out", });
+ *
+ * @example
+ * getScrollTimelineTwoTweens(elem, { opacity: 1, duration: 1, delay: 0.2 }, { scale: 0.7, duration: 1, ease: "circ.out", }, "-=1.5");
+ */
+function getScrollTimelineTwoTweens(elem, gsapToParams = {}, gsapFromParams = {}, nextAnimePos = "<") {
   if (elem instanceof HTMLElement) {
+    //The timeline initiation with the ScrollTrigger and its default params...
     const tl = gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.timeline({
       scrollTrigger: {
         trigger: elem,
         start: "top 100%",
         //end: "bottom 85%",
-        toggleActions: "play reverse restart reverse",
+        toggleActions: "restart none none none",
         //markers: true,
         /**
          * preventOverlaps vs fastScrollEnd - should be chosen on of them
          */
         //preventOverlaps: true, //prevent overlapping animations at several trigger animations
-        fastScrollEnd: true // stop previous animation if the scrollTrigger starts animation again...
+        //fastScrollEnd: true, // stop previous animation if the scrollTrigger starts animation again...
+        lazy: true // Waiting for initialization before coming into scope
       }
     });
     tl.to(elem, {
-      opacity: 1,
-      duration: 0.7,
-      delay: 0.2
+      ...gsapToParams
     });
     tl.from(elem, {
-      x: position === "left" ? -80 : 80,
-      y: 80,
-      duration: 1,
-      ease: "circ.out"
-    }, "<");
+      ...gsapFromParams
+    }, nextAnimePos);
     return tl;
   } else {
     console.warn(`at getScrollTimeLinePairLeftRight: the given selector ${elem} is not HTMLElement...`);
     return false;
   }
 }
-function getScrollTimeLineScaleIn(elem) {
-  if (elem instanceof HTMLElement) {
-    const tl = gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.timeline({
-      scrollTrigger: {
-        trigger: elem,
-        start: "top 100%",
-        //end: "bottom 85%",
-        toggleActions: "play reverse restart reverse",
-        //markers: true,
-        /**
-         * preventOverlaps vs fastScrollEnd - should be chosen on of them
-         */
-        //preventOverlaps: true, //prevent overlapping animations at several trigger animations
-        fastScrollEnd: true // stop previous animation if the scrollTrigger starts animation again...
-      }
-    });
-    tl.to(elem, {
-      opacity: 1,
-      duration: 1,
-      delay: 0.2
-    });
-    tl.from(elem, {
-      scale: 0.7,
-      duration: 1.2,
-      ease: "back.out(1)"
-    }, "<");
-    return tl;
-  } else {
-    console.warn(`at getScrollTimeLineScaleIn: the given selector ${elem} is not HTMLElement...`);
-    return false;
+
+/**
+ * It makes animation timeline for each element by selector and returns the Object with the timeline references...
+ * @param {String} selector - the common selector of the target elements in DOM
+ * @param {String} [propKey="tlKey"] - the key part for making unique key of the timeline in the Object to return...
+ * @param {Object} [gsapToParams={}] - params for the first tween with gsap.to
+ * @param {Object} [gsapFromParams={}] - params for the last tween with gsap.from
+ * @param {String} [nextAnimePos="<"] - The position for the second tween, indicating when the animation should start relative to the first.
+ * @return {Object} - Object containing gsap.core.Timeline references in particular properties...
+ */
+function getAllScrollTwoTweens(selector, propKey = "tlKey", gsapToParams = {}, gsapFromParams = {}, nextAnimePos = "<") {
+  const targetElems = gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.utils.toArray(selector);
+  const tlObj = {};
+  if (!targetElems.length) {
+    console.warn(`at getAllScrollTimeLineTwoTweens(): no elements found in DOM with selector: ${selector}...`);
+    return tlObj;
   }
+  targetElems.forEach((elem, index) => {
+    const tlKey = `${propKey}_${index}`;
+    const tl = getScrollTimelineTwoTweens(elem, gsapToParams, gsapFromParams, nextAnimePos);
+    if (tl && hasRealAnimations(tl)) {
+      tlObj[tlKey] = tl;
+    }
+  });
+  return tlObj;
 }
 
 /////// DEV
